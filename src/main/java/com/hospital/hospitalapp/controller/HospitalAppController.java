4cae8513153e4f590d171c568a57e53ffb3010a6
Priyanko Sarkar <priyanko.sarkar1994@gmail.com>
2022-03-19 16:16:03 +0530

    request consent changes

--- src/main/java/com/hospital/hospitalapp/controller/HospitalAppController.java
+++ src/main/java/com/hospital/hospitalapp/controller/HospitalAppController.java
@@ -1,7 +1,35 @@
 package com.hospital.hospitalapp.controller;
 
+import com.hospital.hospitalapp.DTO.ConsentRequestDTO;
+import com.hospital.hospitalapp.service.HospitalAppService;
+import com.hospital.hospitalapp.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HospitalAppController {
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private HospitalAppService hospitalAppService;
+
+    @PostMapping(value="/request-consent")
+    public ResponseEntity<?> requestConsent(@RequestBody ConsentRequestDTO consentreuestdto, @RequestHeader("Authorization") String token){
+        String doctor_id=jwtService.extractID(token);
+        boolean response= hospitalAppService.requestConsent(doctor_id,consentreuestdto);
+        if(response)
+        {
+            return ResponseEntity.ok("Consent Requested successfully");
+        }
+        else{
+            ResponseEntity<String> resp=new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+            return resp;
+        }
+    }
+
 }
--- src/main/java/com/hospital/hospitalapp/service/HospitalAppService.java
+++ src/main/java/com/hospital/hospitalapp/service/HospitalAppService.java
@@ -1,7 +1,41 @@
 package com.hospital.hospitalapp.service;
 
+import com.hospital.hospitalapp.DTO.ConsentRequestDTO;
+import com.hospital.hospitalapp.central.entity.Consent_request;
+import com.hospital.hospitalapp.central.repository.Consent_request_repo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class HospitalAppService {
+    @Autowired
+    private Consent_request_repo consent_request_repo;
+
+    @Value("{Hospital.id}")
+    private String hospital_id;
+
+    public boolean requestConsent(String doctor_id, ConsentRequestDTO consentreuestdto){
+
+        Consent_request consent_request=new Consent_request();
+        consent_request.setConsent_request_id(UUID.randomUUID().toString());
+        consent_request.setPatient_id(consentreuestdto.getPatient_id());
+        consent_request.setDoctor_id(doctor_id);
+        consent_request.setHospital_id(hospital_id);
+        consent_request.setRequest_info(consentreuestdto.getRequest_info());
+        consent_request.setAccess_purpose(consentreuestdto.getAccess_purpose());
+        consent_request.setRequest_status("Pending");
+        consent_request.setCreated_dt(new Date());
+
+        try{
+            consent_request_repo.save(consent_request);
+
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
 }
